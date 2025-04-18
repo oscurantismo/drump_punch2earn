d29e2662d28a5df146b23b366f9f55eaf69d28a9
oscurantismo <lubyatowo@gmail.com>
2025-04-18 00:47:29 -0400

    Update ui_tabs.js

--- js/ui_tabs.js
+++ js/ui_tabs.js
@@ -24,6 +24,7 @@ function showTab(tab, scene = null) {
 
   if (tab === "game") {
     if (!document.getElementById("punch-bar")) renderPunchBar();
+    renderPunchBadge();
     if (scene) showGameUI(scene);
   }
 
