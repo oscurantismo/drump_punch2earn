2d7a9b1bda01dc6b8ab231f1ecdc46e603e83aad
oscurantismo <lubyatowo@gmail.com>
2025-04-21 21:12:18 -0400

    Update ui_tabs.js

--- js/ui_tabs.js
+++ js/ui_tabs.js
@@ -4,7 +4,7 @@ import { createLeaderboardPopup } from "./popups.js";
 import { renderEarnTab, getIncompleteTaskCount } from "./earn_tab.js";
 import { COLORS, ZINDEX } from "./styles.js";
 import { renderTopBar } from "./topbar.js";
-import { renderPunchBar, renderPunchBadge } from "./punchbar.js";
+import { renderPunchBar, renderPunchBadge, fetchPunchGap } from "./punchbar.js";
 
 function showTab(tab, scene = null) {
   window.activeTab = tab;
